0dabdc51a2377090dca92f237587b8d088d5967a
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2022-05-09 11:36:24 +0800

    delete unused model

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -2,14 +2,12 @@
 
 namespace App\Models;
 
-// use App\Models\Detail;
 use App\Models\Stock;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\DB;
 
 class Item extends Model
 {
-    //ITEMS
     protected $fillable = [
         'id', 'item_code', 'brand_name', 'generic_name', 'description', 'indication', 'indikasi',
         'instruction', 'expiry_date', 'purchase_price', 'purchase_uom', 'purchase_quantity', 'stock_level',
@@ -43,11 +41,6 @@ class Item extends Model
     }
 
     public function used_stock(){
-        // $used_stock = Stock::selectRaw("SUM(quantity) as quantity, item_id")
-        //     ->join
-        //     ->where('item_id', $this->id)
-        //     ->where('source', 'purchase')
-        //     ->groupBy('item_id')->first();
         $used_stock = DB::table('order_items as a')
             ->join('orders as b', 'a.order_id', 'b.id')
             ->join('stocks as c', 'a.id', 'c.source_id')
@@ -66,47 +59,5 @@ class Item extends Model
     {
         return $this->hasMany(OrderItem::class, 'myob_product_id', 'id');
     }
-
-    //MYOB
-    // protected $fillable = [
-    //     'ItemID'
-    // ];
-
-    // public function tariff()
-    // {
-    //     return $this->belongsTo(Tariff::class, 'tariff_id', 'id');
-    // }
-
-    // public function detail()
-    // {
-    //     return $this->hasOne(Detail::class, 'myob_product_id', 'ItemNumber');
-    // }
-
-    // public function details($which)
-    // {
-    //     switch ($which) {
-    //         case 1:
-    //             $detail = Detail::where('myob_product_id', $this->ItemNumber)->first();
-    //             return $detail->instruction;
-    //             break;
-    //         case 2:
-    //             $detail = Detail::where('myob_product_id', $this->ItemNumber)->first();
-    //             return $detail->indikasi;
-    //             break;
-    //         case 3:
-    //             $detail = DB::table('myob_product_details as a')->join('frequencies as b', 'b.id', '=', 'a.frequency_id')->select('b.name')->where('a.myob_product_id', $this->ItemNumber)->first();
-    //             return $detail->name;
-    //             break;
-    //         default:
-    //             return null;
-    //             break;
-    //     }
-    // }
-
-    // public function stock_level(){
-    //     $stock_level = Stock::selectRaw("SUM(quantity) as quantity, item_id")->where('item_id', $this->ItemID)->groupBy('item_id')->first();
-    //     return $stock_level;
-    // }
-    // protected $table = 'myob_products';
 
 }
