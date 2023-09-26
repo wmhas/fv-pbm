ba1e10ab268d18cd94626b3872db46a578ef32da
Muhammad Nurhafizi <muhammadnurhafizi3012@gmail.com>
2023-09-26 17:21:03 +0800

    add softdelete to salesperson

--- app/Models/SalesPerson.php
+++ app/Models/SalesPerson.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class SalesPerson extends Model
 {
+    use SoftDeletes;
+    
     protected $table = 'sales_persons';
 
     protected $fillable = ['name'] ;
